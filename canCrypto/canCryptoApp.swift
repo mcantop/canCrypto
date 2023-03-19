8b433b67f619bb1532069f131358a6be8d392aa6
mcantop <maciejcantop@icloud.com>
2023-03-19 22:42:44 +0100

    Initial Commit

---
+++ canCrypto/canCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  canCryptoApp.swift
+//  canCrypto
+//
+//  Created by Maciej on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct canCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
